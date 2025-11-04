442493c4c572f31f54a5c0688b1bb5d45b0b3a5d
Yuliyala <yuliyalapenak@gmail.com>
2025-11-04 11:02:33 +0300

    Initial commit

---
+++ ForestPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestPackApp.swift
+//  ForestPack
+//
+//  Created by Yuliya Lapenak on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ForestPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
